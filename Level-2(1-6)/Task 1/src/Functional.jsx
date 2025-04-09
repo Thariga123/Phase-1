b3411ca2c41daa150fa72dc92e219e4e1f93428a
Thariga123 <tharigashrinithi@gmail.com>
2025-04-09 10:38:35 +0530

    Delete Level-2(1-6) directory

--- Level-2(1-6)/Task 1/src/Functional.jsx
+++
@@ -1,9 +0,0 @@
-import './Functional.css';
-
-function Functional(){
-    return(
-        <h1>Hello from a functional component!</h1>
-    )
-}
-
-export default Functional
--- Level-2(1-6)/Task 2/src/Components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Hello extends Component{
-    render(){
-        return <h1> Hello from a class component!</h1>
-    }
-}
-
-export default Hello
